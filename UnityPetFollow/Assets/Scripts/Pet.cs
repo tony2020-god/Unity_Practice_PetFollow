45dbb1420577cd7e3b8f9b1e8aecbd97ed493575
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2020-12-01 16:19:56 +0800

    完成寵物移動

---
+++ UnityPetFollow/Assets/Scripts/Pet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    [Header("目標")]
+    public Transform target;
+    [Header("追蹤速度"), Range(0, 100)]
+    public float speed = 1.5f;
+ 
+
+    private void Track()
+    {
+        Vector3 posA = transform.position;    //取得攝像機座標
+        Vector3 posB = target.position;       //取得目標座標
+        
+        //一禎的時間 Time.deltaTime
+        posA = Vector3.Lerp(posA, posB, speed * Time.deltaTime);//插值
+
+        transform.position = posA;            //攝影機座標 = A點
+    }
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
